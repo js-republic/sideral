29038861823231a78fcf3cb6321bfa9d074d99f5
Christophe Brochard <christophe.brochard.prestataire@sfr.com>
2016-11-25 17:53:05 +0100

    feat(hub): First implementation of HUB
    First implementation of HUB including server routing and react components

---
+++ features/hub/client/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hexagon from "react-hexagon";
+
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <div className="pt-dark">
+            </div>
+        );
+    }
+}
