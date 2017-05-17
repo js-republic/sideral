89ed8af728528ffa68f99ab6b0808d752b8c62a0
Julien Teyteau <iuliust@gmail.com>
2017-05-18 01:15:07 +0200

    ref(modules) add index module files

---
+++ src/Tool/index.ts
@@ -0,0 +1,9 @@
+export * from './Body';
+
+export * from './Enum';
+export * from './Signal';
+export * from './Skill';
+export * from './SkillManager';
+export * from './Timer';
+export * from './Util';
+export * from './Wall';
